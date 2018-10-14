40f1dc058057c5c8274544f05b58737383da69ed
tanxin <tanxin@homeserver.com>
2018-10-14 23:52:03 +0800

    1.Merge [discovery server] and [configuration server]

--- discovery/src/main/java/com/ray/service/discovery/DiscoveryApplication.java
+++ discovery/src/main/java/com/ray/service/discovery/DiscoveryApplication.java
@@ -2,9 +2,11 @@ package com.ray.service.discovery;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @EnableEurekaServer
+@EnableConfigServer
 @SpringBootApplication
 public class DiscoveryApplication {
 
