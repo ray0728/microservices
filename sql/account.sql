7a98a12ac26202e7869a171a74f1636816b79417
tanxin <51101661@qq.com>
2019-01-05 23:34:31 +0800

    account sql配置脚本

---
+++ sql/account.sql
@@ -0,0 +1,156 @@
+-- MySQL dump 10.13  Distrib 5.7.24, for Linux (x86_64)
+--
+-- Host: 127.0.0.1    Database: account
+-- ------------------------------------------------------
+-- Server version	5.7.24-0ubuntu0.18.04.1
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `t_account`
+--
+
+DROP TABLE IF EXISTS `t_account`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `t_account` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(45) DEFAULT NULL,
+  `password` varchar(60) DEFAULT NULL,
+  `status` int(11) DEFAULT '0',
+  `first_time` bigint(20) DEFAULT '0',
+  `times` int(11) DEFAULT '0',
+  `last_login` bigint(20) DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=armscii8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `t_account`
+--
+
+LOCK TABLES `t_account` WRITE;
+/*!40000 ALTER TABLE `t_account` DISABLE KEYS */;
+INSERT INTO `t_account` VALUES (5,'txtx','$2a$10$X.T0ZsTmgUZ1oIsxgO3gl.Q28Syzod8yuQs3./L1uOqRFYzt5rA0W',0,20190105022804,0,0),(6,'xx','$2a$10$035zozhft7MY6mZKp7I7zuzvgp5NpMO4J/cg.Qm8eV77KFHAWyj.S',0,20190105023207,0,0);
+/*!40000 ALTER TABLE `t_account` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `t_account_role_map`
+--
+
+DROP TABLE IF EXISTS `t_account_role_map`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `t_account_role_map` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `uid` int(11) NOT NULL,
+  `rid` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=armscii8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `t_account_role_map`
+--
+
+LOCK TABLES `t_account_role_map` WRITE;
+/*!40000 ALTER TABLE `t_account_role_map` DISABLE KEYS */;
+INSERT INTO `t_account_role_map` VALUES (2,5,7),(3,6,2);
+/*!40000 ALTER TABLE `t_account_role_map` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `t_group`
+--
+
+DROP TABLE IF EXISTS `t_group`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `t_group` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(45) DEFAULT NULL,
+  `desc` varchar(200) DEFAULT NULL,
+  `type` int(11) DEFAULT NULL,
+  `level` int(11) DEFAULT NULL,
+  `admin_uid` int(11) DEFAULT NULL,
+  `create_date` bigint(20) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `t_group`
+--
+
+LOCK TABLES `t_group` WRITE;
+/*!40000 ALTER TABLE `t_group` DISABLE KEYS */;
+/*!40000 ALTER TABLE `t_group` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `t_group_member`
+--
+
+DROP TABLE IF EXISTS `t_group_member`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `t_group_member` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `gid` int(11) DEFAULT NULL,
+  `uid` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `t_group_member`
+--
+
+LOCK TABLES `t_group_member` WRITE;
+/*!40000 ALTER TABLE `t_group_member` DISABLE KEYS */;
+/*!40000 ALTER TABLE `t_group_member` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `t_role`
+--
+
+DROP TABLE IF EXISTS `t_role`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `t_role` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `description` varchar(45) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=armscii8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `t_role`
+--
+
+LOCK TABLES `t_role` WRITE;
+/*!40000 ALTER TABLE `t_role` DISABLE KEYS */;
+/*!40000 ALTER TABLE `t_role` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2019-01-05 23:32:31
