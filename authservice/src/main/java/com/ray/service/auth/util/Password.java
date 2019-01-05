18a9dbe8154d0e39b6d73ae0dd6c72c969e0631b
tanxin <51101661@qq.com>
2019-01-05 23:07:44 +0800

    移除过时的工程

--- authservice/src/main/java/com/ray/service/auth/util/Password.java
+++
@@ -1,9 +0,0 @@
-package com.ray.service.auth.util;
-
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-
-public class Password {
-    public static String crypt(String str) {
-        return new BCryptPasswordEncoder().encode(str.trim());
-    }
-}
