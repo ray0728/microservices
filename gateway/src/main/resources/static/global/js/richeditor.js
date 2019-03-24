2ad683d49958b023580633f9b21527e4dcaebb30
tanxin <51101661@qq.com>
2019-03-24 23:26:55 +0800

    little change

--- gateway/src/main/resources/static/global/js/richeditor.js
+++ gateway/src/main/resources/static/global/js/richeditor.js
@@ -1,20 +1,39 @@
 function initEditor(info) {
     console.log(info);
     $('#summernote').summernote({
-        placeholder:info,
+        placeholder: info,
         height: 400,
         fontSizes: ['12', '14', '16', '18', '24', '36', '48'],
         toolbar: [
-            ['fontname',['fontname']],
+            ['fontname', ['fontname']],
             ['style', ['bold', 'italic', 'underline', 'clear']],
             ['fontsize', ['fontsize']],
             ['color', ['color']],
             ['para', ['ul', 'ol', 'paragraph']],
             ['height', ['height']],
-            ['insert',['picture','video','link','table','hr']],
-            ['misc',['fullscreen','undo','redo']]
+            ['insert', ['picture', 'video', 'link', 'table', 'hr']],
+            ['misc', ['fullscreen', 'undo', 'redo']]
         ],
-        disableDragAndDrop:true,
-        shortcut:false
+        disableDragAndDrop: true,
+        shortcut: false
     });
 };
+
+function addcategory(object) {
+    var dialog = $(object).parent().parent();
+    var input = dialog.find('input[id="classifier"]');
+    var userdefcategory = input.val();
+    var shoudadd = true;
+    $("#category").each(function () {
+        console.log($(this).text());
+        if ($(this).text() == userdefcategory) {
+            shoudadd = false;
+        }
+    });
+    if (shoudadd) {
+        var newoption = new Option(userdefcategory, userdefcategory);
+        // $(newoption).html("option text");
+        $('#category').append(newoption);
+    }
+    input.val("");
+}
