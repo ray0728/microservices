608be7d9377d58a43dc863f1f2835617bd5d2677
tanxin <51101661@qq.com>
2019-06-15 23:30:39 +0800

    support form login

--- auth/src/main/java/com/rcircle/service/auth/security/WebSecurityConfigurer.java
+++ auth/src/main/java/com/rcircle/service/auth/security/WebSecurityConfigurer.java
@@ -47,7 +47,7 @@ public class WebSecurityConfigurer extends WebSecurityConfigurerAdapter {
                 .httpBasic()
                 .and()
                 .formLogin()
-//                .disable()
+                .disable()
         ;
     }
 
