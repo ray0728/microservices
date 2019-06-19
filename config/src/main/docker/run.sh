540b6c68c0204e34b4a3b7788aeeee87229ef1c4
tanxin <51101661@qq.com>
2019-06-19 23:45:24 +0800

    update Docker files

--- config/src/main/docker/run.sh
+++ config/src/main/docker/run.sh
@@ -1,4 +1,6 @@
 echo "********************************************************"
 echo "Starting Configuration Server"
 echo "********************************************************"
-java -jar /usr/local/configserver/@project.build.finalName@.jar
+java -Djava.security.egd=file:/dev/./urandom -Dserver.port=$CONFIGSERVER_PORT   \
+     -Deureka.client.serviceUrl.defaultZone=$EUREKASERVER_URI \
+     -jar /usr/local/server/@project.build.finalName@.jar
--- discovery/src/main/docker/run.sh
+++ discovery/src/main/docker/run.sh
@@ -1,5 +1,14 @@
 #!/bin/sh
 echo "********************************************************"
+echo "Waiting for the configuration server to start on port $CONFIGSERVER_PORT"
+echo "********************************************************"
+while ! `nc -z configserver $CONFIGSERVER_PORT`; do sleep 3; done
+echo "*******  Configuration Server has started"
+
+echo "********************************************************"
 echo "Starting the Eureka Server"
 echo "********************************************************"
-java -Djava.security.egd=file:/dev/./urandom -jar /usr/local/discoveryserver/@project.build.finalName@.jar
+java -Djava.security.egd=file:/dev/./urandom                \
+     -Dspring.cloud.config.uri=$CONFIGSERVER_URI            \
+     -Dspring.profiles.active=$PROFILE                      \
+-jar /usr/local/server/@project.build.finalName@.jar
