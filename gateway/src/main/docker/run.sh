ae0579057c1d8bb4cc2fa2dca7d4ef860aa4b143
tanxin <51101661@qq.com>
2019-06-27 00:01:42 +0800

    remove bootstrap, use docker-compose to start service with base configuration

--- gateway/src/main/docker/run.sh
+++ gateway/src/main/docker/run.sh
@@ -12,16 +12,16 @@ while ! `nc -z discoveryserver $EUREKASERVER_PORT`; do sleep 3; done
 echo "******* Eureka Server has started"
 
 echo "********************************************************"
-echo "Waiting for the ZIPKIN server to start  on port $ZIPKIN_PORT"
+echo "Waiting for the account server to start  on port $ACCOUNT_PORT"
 echo "********************************************************"
-while ! `nc -z zipkin $ZIPKIN_PORT`; do sleep 10; done
-echo "******* ZIPKIN has started"
+while ! `nc -z accountserver $ACCOUNT_PORT`; do sleep 10; done
+echo "******* ACCOUNT has started"
 
 echo "********************************************************"
-echo "Waiting for the REDIS server to start  on port $REDIS_PORT"
+echo "Waiting for the resource server to start  on port $RESOURCE_PORT"
 echo "********************************************************"
-while ! `nc -z redis $REDIS_PORT`; do sleep 10; done
-echo "******* REDIS has started"
+while ! `nc -z resourceserver $RESOURCE_PORT`; do sleep 10; done
+echo "******* RESOURCE has started"
 
 
 echo "********************************************************"
