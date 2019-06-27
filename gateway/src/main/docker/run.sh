575a839c81eec6f47cc9a1efe59807ba62e9937d
tanxin <51101661@qq.com>
2019-06-27 23:18:45 +0800

    update DOCKER files and clean application.properties

--- gateway/src/main/docker/run.sh
+++ gateway/src/main/docker/run.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 echo "********************************************************"
+echo "Waiting for the zipkin server to start on port $ZIPKIN_PORT"
+echo "********************************************************"
+while ! `nc -z zipkin $ZIPKIN_PORT`; do sleep 3; done
+echo "*******  Zipkin Server has started"
+
+echo "********************************************************"
 echo "Waiting for the configuration server to start on port $CONFIGSERVER_PORT"
 echo "********************************************************"
 while ! `nc -z configserver $CONFIGSERVER_PORT`; do sleep 3; done
@@ -25,11 +31,12 @@ echo "******* RESOURCE has started"
 
 
 echo "********************************************************"
-echo "Starting the Auth Server"
+echo "Starting the Gateway Server"
 echo "********************************************************"
 java -Djava.security.egd=file:/dev/./urandom                \
      -Dspring.cloud.config.uri=$CONFIGSERVER_URI            \
      -Deureka.client.serviceUrl.defaultZone=$EUREKASERVER_URI \
-     -Dspring.redis.host=$REDIS_URI                         \
+     -Dspring.kafka.bootstrap-servers=$KAFKA_URI            \
+     -Dspring.zipkin.base-url=$ZIPKIN_URI                   \
      -Dspring.profiles.active=$PROFILE                      \
 -jar /usr/local/server/@project.build.finalName@.jar
