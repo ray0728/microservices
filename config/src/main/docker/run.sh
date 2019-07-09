3882d0090607c0ab38328280a3e9a9a4ae13f1db
tanxin <51101661@qq.com>
2019-07-09 22:41:00 +0800

    move basic settings to docker-compose.yml

--- config/src/main/docker/run.sh
+++ config/src/main/docker/run.sh
@@ -1,6 +1,12 @@
 echo "********************************************************"
 echo "Starting Configuration Server"
 echo "********************************************************"
-java -Djava.security.egd=file:/dev/./urandom -Dserver.port=$CONFIGSERVER_PORT   \
-     -Deureka.client.serviceUrl.defaultZone=$EUREKASERVER_URI \
+java -Djava.security.egd=file:/dev/./urandom                         \
+     -Dserver.port=$CONFIGSERVER_PORT                                \
+     -Deureka.client.serviceUrl.defaultZone=$EUREKASERVER_URI        \
+     -Dspring.cloud.config.server.git.uri=$GIT_URI                   \
+     -Dspring.cloud.config.server.git.search-paths=$GIT_SEARCH_PATH  \
+     -Dspring.cloud.config.server.git.username=$GIT_USERNAME         \
+     -Dspring.cloud.config.server.git.password=$GIT_PASSWD           \
+     -Dlocal.save.dir=$SAVE_DIR                                      \
      -jar /usr/local/server/@project.build.finalName@.jar
