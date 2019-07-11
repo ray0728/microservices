450a2c93487763dd7d92e29eea80895368226073
tanxin <51101661@qq.com>
2019-07-11 23:31:09 +0800

    fix some bug for kafka(docker). cannot push/pull message to/from kafka

--- message/src/main/java/com/rcircle/service/message/events/source/NewsBean.java
+++ message/src/main/java/com/rcircle/service/message/events/source/NewsBean.java
@@ -4,8 +4,10 @@ import com.rcircle.service.message.events.models.BreakNews;
 import com.rcircle.service.message.events.models.News;
 import com.rcircle.service.message.utils.SimpleDate;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.MessageHeaders;
 import org.springframework.messaging.support.MessageBuilder;
 import org.springframework.stereotype.Component;
+import org.springframework.util.MimeTypeUtils;
 
 @Component
 public class NewsBean {
@@ -23,15 +25,15 @@ public class NewsBean {
         breakNews.setUrl(url);
         breakNews.setAuthor(author);
         breakNews.setDate(SimpleDate.getUTCTime());
-        source.output().send(MessageBuilder.withPayload(breakNews).build());
+        source.output().send(MessageBuilder.withPayload(breakNews).setHeader(MessageHeaders.CONTENT_TYPE, MimeTypeUtils.APPLICATION_JSON).build());
     }
 
-    public void sendNews(String title, String author, String url){
+    public void sendNews(String title, String author, String url) {
         News news = new News();
         news.setAuthor(author);
         news.setDate(SimpleDate.getUTCTime());
         news.setTitle(title);
         news.setUrl(url);
-        source.output().send(MessageBuilder.withPayload(news).build());
+        source.output().send(MessageBuilder.withPayload(news).setHeader(MessageHeaders.CONTENT_TYPE, MimeTypeUtils.APPLICATION_JSON).build());
     }
 }
